91a5a28a0fb28b8f419897b012835c21c0113106
Giovanni Chajon <giochajon@gmail.com>
2019-05-07 19:53:50 -0600

    added little calgary tower on map

--- cgyhood/src/components/Smap.js
+++ cgyhood/src/components/Smap.js
@@ -18,8 +18,7 @@ const Smap = compose(
      * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
      */
     googleMapURL:
-     // process.env.REACT_APP_GOOGLEMAPS_API_KEY,
-    "https://maps.googleapis.com/maps/api/js?key=AIzaSyDpPmriUT2fP_TQ0cIN4AHPqN9_3c1tXhs&v=3.exp&libraries=geometry,drawing,places",
+    process.env.REACT_APP_GOOGLEMAPS_API_KEY,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px`, width: '300px'}} />,
     mapElement: <div style={{ height: `90%` }} />
@@ -43,22 +42,5 @@ const Smap = compose(
 ));
 
 console.log('hola',process.env.REACT_APP_GOOGLEMAPS_API_KEY)
-/*  ANTES MARKER
-  {props.isMarkerShown && (
-      <Marker position= {{lat:51.044270, lng:-114.062019}} 
-      icon={"http://maps.google.com/mapfiles/ms/icons/blue.png"}
-      label="<CT" />
-    )}
-{process.env.REACT_APP_GOOGLEMAPS_API_KEY}
-
-googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDpPmriUT2fP_TQ0cIN4AHPqN9_3c1tXhs&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `300px`, width: '300px'}} />,
-    mapElement: <div style={{ height: `90%` }} />
-
-  */
-
-
 
 export default Smap; 
