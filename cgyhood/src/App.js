e2398fb48dc345e6cd791fe1381528e150f7d63c
Giovanni Chajon <giochajon@gmail.com>
2019-05-23 19:47:19 -0600

    added particles

--- cgyhood/src/App.js
+++ cgyhood/src/App.js
@@ -4,14 +4,44 @@ import './App.css';
 import CommunityController from './components/communityController.js'
 //import HttpsRedirect from 'react-https-redirect'
 
+import Particles from 'react-particles-js'
+
+const particlesOptions = {
+                    "particles": {
+                      "color": {
+                                "value": "#11aa00"
+                                  },
+                        "line_linked": {
+                                    "color":"#11aa00"
+                                    },
+                        "number": {
+                            "value": 150
+                        },
+                        "size": {
+                            "value": 5
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }
+
+
 
 
 function App() {
   return (
+      
     <div className="App">
+<Particles className='particles' params={particlesOptions}  / >
+      
       <h3>Compare Calgary residential communities:</h3>
       <header className="App-header">
-        
         <CommunityController />
         
         <CommunityController />
