7c68de153703731fad6d14ccd5b5bec423486b6e
Giovanni Chajon <giochajon@gmail.com>
2019-05-25 10:40:22 -0600

    added the card that pulls data for the graph

--- cgyhood/src/components/graphContainer.js
+++ cgyhood/src/components/graphContainer.js
@@ -7,10 +7,42 @@ class GraphController extends React.Component {
 
         this.state = {
         city1:null,
-        city2:null
+        city2:null,
+        data1:null,
+        data2:null
         }
+        //this.data1 = null
+        //this.data2 = null
    
     }
+
+
+    populateCompData(comm1,comm2) {
+        let linka = "https://cgy-node-knex.herokuapp.com/compare/"+comm1+"/"+comm2
+        fetch(linka, {
+        //fetch('http://127.0.0.1:5000/community/', {
+                method: 'GET',
+                mode: 'cors',
+                dataType: 'json'
+            })
+            .then(r => r.json())
+            .then(r => {
+                //this.setState({ dropList: r })
+                console.log(linka,r)
+                //this.data1 = r[0]
+                //this.data2 = r[1]
+                this.setState({ data1: r[0] })
+                this.setState({ data2: r[1] })
+            })
+            .catch(err => { //console.log(err)
+                //this.setState({ dropList: defaultDrop })
+                console.log('default loaded')
+            })
+
+    }
+
+
+
 
     myCallback1 = (citycode) => { 
         this.setState({ city1: citycode });
@@ -22,8 +54,10 @@ class GraphController extends React.Component {
     }
 
         render() {
-        
-          
+            let info = ""
+            if (this.state.data1) {
+            info =  JSON.stringify (this.state.data1) + "and" + JSON.stringify (this.state.data2)
+            }
     
     
             return (
@@ -37,6 +71,10 @@ class GraphController extends React.Component {
     
                 <div className="bor3">
                 <p> {this.state.city1} VS {this.state.city2} </p>
+                <button onClick={() => {this.populateCompData(this.state.city1,this.state.city2)}
+                } > GET DATA</button>
+                
+                {info}
                 </div>
                 </div>
             );
