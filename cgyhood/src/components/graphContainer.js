bb1ac576cd57a67b883a6ef804b799c380e03b65
Alex <alexjinga@gmail.com>
2019-05-29 11:52:54 -0600

    corrected graphs details & automatic graph generation

--- cgyhood/src/components/graphContainer.js
+++ cgyhood/src/components/graphContainer.js
@@ -33,8 +33,9 @@ class GraphController extends React.Component {
             .then(r => {
                this.setState({ data1: r[0] })
                this.setState({ data2: r[1] })
-               
+            //    this.populateCompData(this.state.city1,this.state.city2)
                this.displayGraph()
+               
             })
             .catch(err => { 
                 console.log('err:',err)
@@ -57,19 +58,23 @@ class GraphController extends React.Component {
     displayGraph = () => {
         let a = []
         a.push (<div className="App">
-        <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.res_cnt} val2={this.state.data2.res_cnt} mtitle="Population" legendPosition="bottom"/>
-        </div>)
-
-        a.push (<div className="App">
-        <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.dwell_cnt} val2={this.state.data2.dwell_cnt} mtitle="Dwellings" legendPosition="bottom"/>
-        </div>)
-
-        a.push (<div className="App">
-            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.ownshp_cnt} val2={this.state.data2.ownshp_cnt} mtitle="Home Owners" legendPosition="bottom"/>
+            <h2> {this.state.city1} VS {this.state.city2}</h2>
             </div>)
 
         a.push (<div className="App">
-            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.prsch_chld} val2={this.state.data2.prsch_chld} mtitle="Pre-school Children" legendPosition="bottom"/>
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.res_cnt} val2={this.state.data2.res_cnt} mtitle="Population" legendPosition="bottom"/>
+            </div>)
+
+        a.push (<div className="App">
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Dwellings" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.dwell_cnt} val2={this.state.data2.dwell_cnt} mtitle="Dwellings" legendPosition="bottom"/>
+            </div>)
+
+        a.push (<div className="App">
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Home Owners" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.ownshp_cnt} val2={this.state.data2.ownshp_cnt} mtitle="Home Owners" legendPosition="bottom"/>
+            </div>)
+
+        a.push (<div className="App">
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Pre-school Children" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.prsch_chld} val2={this.state.data2.prsch_chld} mtitle="Pre-school Children" legendPosition="bottom"/>
             </div>)
 
         return a 
@@ -92,13 +97,14 @@ class GraphController extends React.Component {
             comms = (
                 <div className="App-header">       
                     <CommunityController callbackFromParent={this.myCallback1} />
-                    <CommunityController callbackFromParent={this.myCallback2}/> 
+                    <CommunityController callbackFromParent={this.myCallback2} /> 
                 </div> 
             );
             if (this.toggle>=2) {
-                info = <IosRefresh onClick={
-                    () => {this.populateCompData(this.state.city1,this.state.city2)} 
-                } fontSize="60px" color="#347eff" rotate={true} />
+                this.populateCompData(this.state.city1,this.state.city2)
+                // info = <IosRefresh onClick={
+                //     () => {this.populateCompData(this.state.city1,this.state.city2)} 
+                // } fontSize="60px" color="#347eff" rotate={true} />
             } else {
                 info = this.displayGraph()
             }
@@ -115,7 +121,7 @@ class GraphController extends React.Component {
                             shake={true} fontSize="20px" color="Blue" 
                         /> 
                     </p>
-                    <p> {this.state.city1} VS {this.state.city2} </p>
+                    {/* <p> {this.state.city1} VS {this.state.city2} </p> */}
                     {info}        
                 </div>
             </div>
