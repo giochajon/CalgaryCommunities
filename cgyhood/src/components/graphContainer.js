69a1b7144e9dc21a657eb53aa431b7ab30390654
Alex <alexjinga@gmail.com>
2019-05-29 10:19:45 -0600

    latest version

--- cgyhood/src/components/graphContainer.js
+++ cgyhood/src/components/graphContainer.js
@@ -13,8 +13,8 @@ class GraphController extends React.Component {
         this.state = {
         city1:null,
         city2:null,
-         data1:{"comm_code":"ALT","name":"ALTADORE","res_cnt":"6831","dwell_cnt":"3218","prsch_chld":"677","ownshp_cnt":"1945","hotel_cnt":"2"},
-         data2:{"comm_code":"SNA","name":"SUNALTA","res_cnt":"3268","dwell_cnt":"2172","prsch_chld":"150","ownshp_cnt":"469","hotel_cnt":"1"}
+         data1:"",
+         data2:"",
         }
         this.toggle = 0
         this.showAbout = false
@@ -55,24 +55,24 @@ class GraphController extends React.Component {
     }
 
     displayGraph = () => {
-      let a = []
-      a.push (<div className="App">
-      <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.res_cnt} val2={this.state.data2.res_cnt} mtitle="Population" legendPosition="bottom"/>
-      </div>)
+        let a = []
+        a.push (<div className="App">
+        <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.res_cnt} val2={this.state.data2.res_cnt} mtitle="Population" legendPosition="bottom"/>
+        </div>)
 
-    a.push (<div className="App">
-    <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.dwell_cnt} val2={this.state.data2.dwell_cnt} mtitle="Dwellings" legendPosition="bottom"/>
-    </div>)
+        a.push (<div className="App">
+        <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.dwell_cnt} val2={this.state.data2.dwell_cnt} mtitle="Dwellings" legendPosition="bottom"/>
+        </div>)
 
-a.push (<div className="App">
-    <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.ownshp_cnt} val2={this.state.data2.ownshp_cnt} mtitle="Home Owners" legendPosition="bottom"/>
-    </div>)
+        a.push (<div className="App">
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.ownshp_cnt} val2={this.state.data2.ownshp_cnt} mtitle="Home Owners" legendPosition="bottom"/>
+            </div>)
 
-a.push (<div className="App">
-    <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.prsch_chld} val2={this.state.data2.prsch_chld} mtitle="Pre-school Children" legendPosition="bottom"/>
-    </div>)
+        a.push (<div className="App">
+            <Chart key={this.state.data1.name} key={this.state.data2.name} tit="Population" lb1={this.state.data1.name} lb2 ={this.state.data2.name} val1={this.state.data1.prsch_chld} val2={this.state.data2.prsch_chld} mtitle="Pre-school Children" legendPosition="bottom"/>
+            </div>)
 
-    return a 
+        return a 
     } 
 
     aboutWindow=()=>{
@@ -83,51 +83,44 @@ a.push (<div className="App">
     }   
 
 
-        render() {
-            let info = null
-            let comms = null 
-            if (this.showAbout === true) {comms = null; info = <About/>  } 
-            else {
-
-comms =      (    <div className="App-header">       <CommunityController callbackFromParent={this.myCallback1} />
-        <CommunityController callbackFromParent={this.myCallback2}/> </div> );
-
-
-
-            if (this.toggle>=2) {
-                info = <IosRefresh onClick={() => {
-                this.populateCompData(this.state.city1,this.state.city2)
-             } 
-             } fontSize="60px" color="#347eff" rotate={true} />
-  
-            }
-             
-            else {info = this.displayGraph()}
-        }
-             
-            
-    
-    
-            return (
-                <div >
-                
-                   
-                {comms}
-    
-                
-    
-                <div className="bor3">
-               <p className="descrip"> <IosInformationCircleOutline onClick={() => {this.aboutWindow() }} shake={true} fontSize="20px" color="Blue" /> </p>
-                <p> {this.state.city1} VS {this.state.city2} </p>
-                {info}
-                
-                
-                
-                </div>
-                </div>
+    render() {
+        let info = null
+        let comms = null 
+        if (this.showAbout === true) {
+            comms = null; info = <About/>  
+        } else {
+            comms = (
+                <div className="App-header">       
+                    <CommunityController callbackFromParent={this.myCallback1} />
+                    <CommunityController callbackFromParent={this.myCallback2}/> 
+                </div> 
             );
+            if (this.toggle>=2) {
+                info = <IosRefresh onClick={
+                    () => {this.populateCompData(this.state.city1,this.state.city2)} 
+                } fontSize="60px" color="#347eff" rotate={true} />
+            } else {
+                info = this.displayGraph()
+            }
         }
+        return (
+            <div>           
+                {comms}
+                <div className="bor3">
+                    <p className="descrip"> 
+                        <IosInformationCircleOutline 
+                            onClick = {
+                                () => {this.aboutWindow() }
+                            } 
+                            shake={true} fontSize="20px" color="Blue" 
+                        /> 
+                    </p>
+                    <p> {this.state.city1} VS {this.state.city2} </p>
+                    {info}        
+                </div>
+            </div>
+        );
     }
- 
+}
 
-    export default GraphController;
+export default GraphController;
