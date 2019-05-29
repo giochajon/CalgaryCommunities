7913175dc7c92ea39d24fe4e93f079f7bdc292a1
Giovanni Chajon <giochajon@gmail.com>
2019-05-28 19:02:24 -0600

    added the formated about component

--- cgyhood/src/components/Chart.js
+++ cgyhood/src/components/Chart.js
@@ -1,80 +1,82 @@
 import React, {Component} from 'react';
 import {HorizontalBar, Doughnut} from 'react-chartjs-2';
+import './Chart.css';
 
 class Chart extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      chartData:props.chartData
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData:props.chartData
+        }
     }
-  }
 
-  static defaultProps = {
-    displayTitle:true,
-    displayLegend: true,
-    legendPosition:'right',
-    location:'City',
-    scale: {
-        Axes:[{
-        barThickness: 20,
-        maxBarThickenss: 25,
-        }]
+    static defaultProps = {
+        displayTitle:true,
+        displayLegend: true,
+        legendPosition:'right',
+        location:'City',
     }
-  }
+
+    ShowBarGraph = () => {
+        return(
+            <div className="chart">
+                <p>test</p>
+                <HorizontalBar
+                    data={this.state.chartData}
+                    width={100}
+                    height={30}
+                    options={{
+                        maintainAspectRatio: true,
+                        title:{ 
+                            display:this.props.displayTitle,
+                            text:'Largest Cities In '+this.props.location,
+                            fontSize:15
+                        },
+                        legend:{
+                            display: false,
+                            // displayLegend:true,
+                            // display:this.props.displayLegend,
+                            // position:this.props.legendPosition
+                        },
+                    }}
+                />
+            </div>
+        )
+    }
+
+
+    ShowDoughnutGraph = () => {
+        return(
+            <div>        
+                <Doughnut
+                    data={this.state.chartData}
+                    options={{
+                        title:{
+                            display:this.props.displayTitle,
+                            text:'Largest Cities In '+this.props.location,
+                            fontSize:15
+                        },
+                        legend:{
+                            display:this.props.displayLegend,
+                            position:this.props.legendPosition
+                        }
+                    }}
+                />
+            </div>
+        )
+    }
+
 
   render(){
+    console.log("state in Chart class", this.state.chartData)
     return (
-      <div className="chart">
-        <HorizontalBar
-          data={this.state.chartData}
-          options={{
+        <div>
+            <div className = 'GraphContainer'>
+            {this.ShowBarGraph()}
+            </div>
 
-            title:{
-              display:this.props.displayTitle,
-              text:'Largest Cities In '+this.props.location,
-              fontSize:15
-            },
-            
-            legend:{
-              displayLegend:false,
-              display:this.props.displayLegend,
-              position:this.props.legendPosition
-            }
-          }}
-        />
-
-        <HorizontalBar
-          data={this.state.chartData}
-          options={{
-
-            title:{
-              display:this.props.displayTitle,
-              text:'Largest Cities In '+this.props.location,
-              fontSize:15
-            },
-            
-            legend:{
-              display:this.props.displayLegend,
-              position:this.props.legendPosition
-            }
-          }}
-        />
-
-        <Doughnut
-          data={this.state.chartData}
-          options={{
-            title:{
-              display:this.props.displayTitle,
-              text:'Largest Cities In '+this.props.location,
-              fontSize:15
-            },
-            legend:{
-              display:this.props.displayLegend,
-              position:this.props.legendPosition
-            }
-          }}
-        />
-      </div>
+            {/* {this.ShowDoughnutGraph ()} */}
+        </div>
     )
   }
 }
--- cgyhood/src/components/graphContainer.js
+++ cgyhood/src/components/graphContainer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import CommunityController from './communityController.js'
+import Chart from './Chart'
+
 
 class GraphController extends React.Component {
     constructor(props) {
@@ -8,14 +10,58 @@ class GraphController extends React.Component {
         this.state = {
         city1:null,
         city2:null,
-        data1:null,
-        data2:null
+        // data1:null,
+        // data2:null
+        chartData : { 
+            labels: ["data1.name", "data2.name"],
+            datasets:[
+              {
+                label:'',
+                data:[
+                  0,
+                  0,
+                ],
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                ]
+              }
+            ]
+          }
         }
+        
         //this.data1 = null
-        //this.data2 = null
-   
+        //this.data2 = null   
     }
 
+    // componentWillMount(){
+    //     this.getChartData();
+    //   }
+
+    getChartData = (data1, data2) =>{
+        // Ajax calls here
+        console.log ('data1',data1.name)
+        console.log ('data2',data2.name)
+        this.setState({
+          chartData : { 
+            labels: [data1.name, data2.name],
+            datasets:[
+              {
+                label:'',
+                data:[
+                  617594,
+                  181045,
+                ],
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                ]
+              }
+            ]
+          }
+        })
+        console.log ("state chartdata",this.state.chartData)
+      }
 
     populateCompData(comm1,comm2) {
         let linka = "https://cgy-node-knex.herokuapp.com/compare/"+comm1+"/"+comm2
@@ -30,9 +76,8 @@ class GraphController extends React.Component {
                 //this.setState({ dropList: r })
                 console.log(linka,r)
                 //this.data1 = r[0]
-                //this.data2 = r[1]
-                this.setState({ data1: r[0] })
-                this.setState({ data2: r[1] })
+                //this.data2 = r[1]  
+               this.getChartData(r[0], r[1])
             })
             .catch(err => { //console.log(err)
                 //this.setState({ dropList: defaultDrop })
@@ -42,17 +87,23 @@ class GraphController extends React.Component {
 
     }
 
-
-
-
     myCallback1 = (citycode) => { 
         this.setState({ city1: citycode });
+        // this.populateCompData_1(this.state.city1);
+        // console.log (this.state.data1)
     }
 
     myCallback2 = (citycode) => { 
-        
         this.setState({ city2: citycode });
     }
+
+    displayGraph = () => {
+        return(
+            <div className="App">
+                    <Chart chartData={this.state.chartData} location="Calgary" legendPosition="bottom"/>
+            </div>
+        )
+    } 
 
         render() {
             let info = ""
@@ -72,8 +123,13 @@ class GraphController extends React.Component {
     
                 <div className="bor3">
                 <p> {this.state.city1} VS {this.state.city2} </p>
-                <button onClick={() => {this.populateCompData(this.state.city1,this.state.city2)}
+                <button onClick={() => {
+                   this.populateCompData(this.state.city1,this.state.city2)
+                } 
                 } > GET DATA</button>
+
+                {/* {this.displayGraph()} */}
+                <Chart chartData={this.state.chartData} location="Calgary" legendPosition="bottom"/>
                 
                 {info}
                 </div>
